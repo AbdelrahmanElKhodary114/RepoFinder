2df0a4a1ae9ed167c737ad21667bc72f5e5cbb28
Abdulrahman Alaa <abdulrhmanalaa114@gmail.com>
2025-05-27 16:46:07 +0300

    Initial Commit

---
+++ RepoFinder/RepoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoFinderApp.swift
+//  RepoFinder
+//
+//  Created by Abdulrahman Alaa on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RepoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
